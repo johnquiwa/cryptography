25de94d1f7f7206f2794ca63d36a77cc9e7308c1
John  Quiwa <johnquiwacode@gmail.com>
2018-05-28 17:15:02 -0400

    change variables names to be more self-describing

--- creatingEthAddress.js
+++ creatingEthAddress.js
@@ -4,15 +4,15 @@ const ec = new EC('secp256k1');
 const keccak256 = require('js-sha3').keccak256;
 
 // Private Key and Public Ethereum address (from Ganache)
-const privateKey = 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3';
-const publicAddress = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
+const givenPrivateKey = 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3';
+const givenEthAddress = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
 
 // Get secp256k1 generator point
 const generatorPoint = ec.g;
 
 // Public Key Coordinates calculated via Elliptic Curve Multiplication
 // PublicKeyCoordinates = privateKey * generatorPoint
-const pubKeyCoordinates = generatorPoint.mul(privateKey);
+const pubKeyCoordinates = generatorPoint.mul(givenPrivateKey);
 
 const x = pubKeyCoordinates.getX().toString('hex');
 const y = pubKeyCoordinates.getY().toString('hex');
@@ -31,9 +31,8 @@ const ethAddressBuffer = new Buffer(hashOfPublicKey, 'hex');
 const ethAddress = ethAddressBuffer.slice(-20).toString('hex');
 const ethAddressWithPrefix = `0x${ethAddress}`;
 
-
 // Ethereum Address derived from private key
 // is equal to the given address
-console.log(ethAddressWithPrefix === givenAddress);
+console.log(ethAddressWithPrefix === givenEthAddress);
 
 
